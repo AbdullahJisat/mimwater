d3f6212ffbdc9ce2e766e2364e38cf3e293e7415
AbdullahJisat <sijisat@gmail.com>
2022-05-09 14:34:27 +0600

    save salesman

---
+++ database/migrations/2022_05_08_063936_create_salesmen_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salesmen', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('phone')->unique();
+            $table->string('location')->nullable();
+            $table->string('nid')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('image')->nullable();
+            $table->tinyInteger('status')->nullable()->comment('1 - active & 0 - inactive');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salesmen');
+    }
+};
