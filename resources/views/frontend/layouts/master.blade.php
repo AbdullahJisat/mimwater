8e719ccf57edd7c8e2475509e523465e1161f793
AbdullahJisat <sijisat@gmail.com>
2022-06-29 14:57:47 +0600

    logs

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -3,17 +3,17 @@
 
 <head>
     @include('frontend.layouts.partials._head')
-    @include('frontend\layouts\partials\_css')
+    @include('frontend.layouts.partials._css')
 
 </head>
 
 <body style="background-color: #ddefff; margin: 0;padding: 0;">
-    @include('frontend\layouts\partials\_nav')
+    @include('frontend.layouts.partials._nav')
 
-    {{-- @include('frontend\layouts\partials\_contnt') --}}
+    {{-- @include('frontend.layouts.partials._contnt') --}}
     @yield('content')
 
-    @include('frontend\layouts\partials\_footer')
+    @include('frontend.layouts.partials._footer')
 
 
     @include('frontend.layouts.partials._script')
